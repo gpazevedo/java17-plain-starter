3c073b01bad3c8d10b848ad103c75ad486e13420
Gustavo Azevedo <Gustavo.Peixoto.de.Azevedo@gmail.com>
2022-11-12 09:34:38 -0300

    Java 17 Plain Starter

---
+++ src/test/java/br/com/gruposbf/burnwood/KafkaTwinTest.java
@@ -0,0 +1,17 @@
+package br.com.gruposbf.burnwood;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class KafkaTwinTest
+{
+    @Test
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
